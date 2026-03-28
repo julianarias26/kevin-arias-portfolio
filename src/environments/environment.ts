cfde4e76d8c8bb7b4a0c363ed3fdc587d1d61746
KevinArias02610 <kjulianr41@gmail.com>
2026-03-28 00:37:36 -0500

    Se ajusta .gitignore

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  supabaseUrl:  'https://crtelslwfxwbbobpbsbn.supabase.co',
+  supabaseKey:  'sb_publishable_wAmCTnW0M6W7iY2F-VUSQQ_nG0oVNoT',
+};
